1f1f506326309a6f35b411492d4434c07547861d
alee4938 <70536080+alee4938@users.noreply.github.com>
2023-10-20 23:48:53 +0000

    Co-authored-by: JeffersonianDemocracy <JeffersonianDemocracy@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 import questions
 
 def main():
-    #print(questions.name_president())
-    print(questions.name_capital())
+    print(questions.name_president())
+    #print(questions.name_capital())
 
 if __name__ == "__main__":
     main()
--- questions.py
+++ questions.py
@@ -4,13 +4,29 @@ import pycountry
 
 app_id = "28KQW5-RVLGPL3KAH"
 
+
+
 def name_president() -> list:
-    question = f"What was the name of the {random.randint(1, 46)} president of the united states?"
-    ans = inquire(app_id, question).split(" (")
-    return [question, ans[0]]
+    answer = list()
+
+    for answers in range(4):
+        random_num = random.randint(1, 46)
+        question = f"What is the name of the {random_num} president of the United States?"
+        if answers == 0:
+            answer.append(question)
+        ans = inquire(app_id, question).split(" (")
+        answer.append(ans[0])
+    return answer
 
 def name_capital():
-    country = list(pycountry.countries)[random.randint(0,192)].name
-    question = f"What is the capital of {country}?"
-    ans = inquire(app_id, question).split(",")
-    return [question, ans[0]]
+    countryList = list(pycountry.countries)
+    print(len(countryList))
+    answers = random.sample(range(249),4)
+    questions = []
+    ans = []
+    for counter in range(4):
+        country = list(pycountry.countries)[answers[counter]].name
+        questions.append(f"What is the capital of {country}?")
+        ans.append(inquire(app_id, questions[counter]).split(","))
+    return [questions[0], ans[0][-2].lstrip(), ans[1][-2].lstrip(), ans[2][-2].lstrip(), ans[3][-2].lstrip()]
+
