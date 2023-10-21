5fdbb441547e5d0d869627bad1d668b39780a997
alee4938 <70536080+alee4938@users.noreply.github.com>
2023-10-21 00:49:28 +0000

    modified:   app.py 	new file:   templates/index.html 	new file:   templates/layout.html

--- app.py
+++ app.py
@@ -1,5 +1,10 @@
 from flask import Flask, render_template
 
-app = flask(__name__)
+app = Flask(__name__)
 
-@app
+@app.route('/')
+def index():
+    return render_template("index.html")
+
+if __name__ == "__main__":
+    app.run()
