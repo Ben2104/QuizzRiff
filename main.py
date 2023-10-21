b27e964cd5256880706f4381500f22697d954ffc
alee4938 <70536080+alee4938@users.noreply.github.com>
2023-10-21 01:18:00 +0000

    Co-authored-by: JeffersonianDemocracy <JeffersonianDemocracy@users.noreply.github.com> Co-authored-by: Khoi Hoang Do <dohoangkhoi341@gmail.com> Co-authored-by: LinhHuynh2403 <LinhHuynh2403@users.noreply.github.com>

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
 import questions
+from flask import Flask, render_template
 
 def main():
-    print(questions.name_president())
-    #print(questions.name_capital())
+    quiz = questions.name_president()
+    #quiz = questions.name_capital()
+    print(quiz)
+    render_template("index.html",)
+    
+
 
 if __name__ == "__main__":
     main()
