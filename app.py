5179fc0703bc5d0de8d3eb842de56cd9a5d6ae50
alee4938 <70536080+alee4938@users.noreply.github.com>
2023-10-21 18:12:30 +0000

    Co-authored-by: JeffersonianDemocracy <JeffersonianDemocracy@users.noreply.github.com> Co-authored-by: LinhHuynh2403 <LinhHuynh2403@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,24 +1,30 @@
-from flask import Flask, render_template, request
-# from flask import Sock
+from flask import Flask, render_template, request, redirect
+from flask_sock import Sock
 import questions
 import random
 import sqlite3
 
 app = Flask(__name__)
-#sock = Sock(app)
+sock = Sock(app)
 
 global score
 score = 0
 global quiz
 quiz = questions.random_question()
+global logged
+logged = False
 
 @app.route("/", methods = ["GET", "POST"])
 def quizs():
     global score
     global quiz
+    global logged
+    if logged == False:
+        return redirect(url_for("/loggin"))
     if request.method == "POST":
         pre_correct = quiz[1]
         submit_button_value = request.form["answers"]
+        print(submit_button_value)
         if submit_button_value == pre_correct:
             score += 1
         quiz = questions.random_question()
@@ -38,15 +44,23 @@ def quizs():
                                answer3=quiz[order[2]],
                                answer4=quiz[order[3]])
 
-"""
+@app.route("/login")
+def login():
+    return render_template("login.html")
+
+#@app.route("/logging")
+
+#def Logging():
+
 @sock.route('/echo')
 def echo(sock):
     global score
     global quiz
-
+    print("echo")
     pre_correct = quiz[1]
     #submit_button_value = request.form["answers"]
     submit_button_value = sock.receive()
+    priint(submit_button_value)
     if submit_button_value == pre_correct:
         score += 1
     quiz = questions.random_question()
@@ -58,7 +72,7 @@ def echo(sock):
                             answer4=quiz[order[3]],
                             score=score,
                             correct_answer=pre_correct)
-"""
+
 
 if __name__ == "__main__":
     app.run()
